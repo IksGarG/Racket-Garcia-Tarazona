b287dd52693f6769b571577176415114e09fde98
Tarazonaa <andres.tara.so@gmail.com>
2023-04-21 22:50:31 -0600

    Comments are created with a

--- DFA/DFA.rkt
+++ DFA/DFA.rkt
@@ -15,7 +15,7 @@ Iker García -
 (struct dfa (func initial accept))
 
 (define (arithmetic-lexer strng)
-  (evaluate-dfa (dfa delta-arithmetic 'start '(paropn parcls int float exp var spa)) strng))
+  (evaluate-dfa (dfa delta-arithmetic 'start '(paropn parcls int float exp var spa comment)) strng))
 
 (define (evaluate-dfa dfa-to-evaluate strng)
   " This function will verify if a string is acceptable by a DFA "
@@ -47,12 +47,8 @@ Iker García -
   (member char '(#\+ #\- #\* #\/ #\= #\^)))
 
 (define (delta-arithmetic state char)
-  " Transition function to validate numbers
-  This function now returns two values:
-   - The new state in the automaton
-   - The token that has been found. Generally false, until we are sure to have found a token
-  Initial state: start
-  Accept states: int float exp "
+  "States accepted: 'start 'paropn 'parcls 'sign 'int 'dot 'float 'e 'e_sign 'exp 'var 'spa 'comment 'inv
+  Comments are created with one bang"
   (case state
     ['start (cond
        [(char-numeric? char) (values 'int #f)]
@@ -61,6 +57,8 @@ Iker García -
        [(eq? char #\_) (values 'var #f)]
        [(eq? char #\() (values 'paropn #f)]
        [(eq? char #\)) (values 'parcls #f)]
+       [(eq? char #\space) (values 'spa #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['paropn (cond
         [(char-numeric? char) (values 'int 'paropn)]
@@ -69,6 +67,7 @@ Iker García -
         [(eq? char #\_) (values 'var 'paropn)]
         [(eq? char #\() (values 'paropn 'paropn)]
         [(eq? char #\)) (values 'parcls 'paropn)])]
+       [(eq? char #\!) (values 'comment #f)]
     ['parcls (cond
         [(char-numeric? char) (values 'int 'parcls)]
         [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'parcls)]
@@ -77,10 +76,12 @@ Iker García -
         [(eq? char #\_) (values 'var 'parcls)]
         [(eq? char #\() (values 'paropn 'parcls)]
         [(eq? char #\)) (values 'parcls 'parcls)])]
+       [(eq? char #\!) (values 'comment #f)]
     ['sign (cond
        [(char-numeric? char) (values 'int #f)]
         [(eq? char #\() (values 'paropn #f)]
         [(eq? char #\)) (values 'parcls #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['int (cond
        [(char-numeric? char) (values 'int #f)]
@@ -90,9 +91,11 @@ Iker García -
        [(eq? char #\space) (values 'spa 'int)]
         [(eq? char #\() (values 'paropn 'int)]
         [(eq? char #\)) (values 'parcls 'int)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['dot (cond
        [(char-numeric? char) (values 'float #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['float (cond
        [(char-numeric? char) (values 'float #f)]
@@ -101,13 +104,16 @@ Iker García -
        [(eq? char #\space) (values 'spa 'float)]
         [(eq? char #\() (values 'paropn 'float)]
         [(eq? char #\)) (values 'parcls 'float)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['e (cond
        [(char-numeric? char) (values 'exp #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [(or (eq? char #\+) (eq? char #\-)) (values 'e_sign #f)]
        [else (values 'inv #f)])]
     ['e_sign (cond
        [(char-numeric? char) (values 'exp #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['exp (cond
        [(char-numeric? char) (values 'exp #f)]
@@ -115,6 +121,7 @@ Iker García -
        [(eq? char #\space) (values 'spa 'exp)]
         [(eq? char #\() (values 'paropn 'exp)]
         [(eq? char #\)) (values 'parcls 'exp)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['var (cond
        [(char-alphabetic? char) (values 'var #f)]
@@ -124,6 +131,7 @@ Iker García -
        [(eq? char #\space) (values 'spa 'var)]
         [(eq? char #\() (values 'paropn 'var)]
         [(eq? char #\)) (values 'parcls 'var)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['op (cond
        [(char-numeric? char) (values 'int 'op)]
@@ -133,12 +141,14 @@ Iker García -
        [(eq? char #\space) (values 'op_spa 'op)]
         [(eq? char #\() (values 'paropn 'op)]
         [(eq? char #\)) (values 'parcls 'op)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
      ['spa (cond
        [(char-operator? char) (values 'op #f)]
        [(eq? char #\space) (values 'spa #f)]
         [(eq? char #\() (values 'paropn #f)]
         [(eq? char #\)) (values 'parcls #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
     ['op_spa (cond
        [(char-numeric? char) (values 'int #f)]
@@ -148,5 +158,9 @@ Iker García -
        [(eq? char #\space) (values 'op_spa #f)]
         [(eq? char #\() (values 'paropn #f)]
         [(eq? char #\)) (values 'parcls #f)]
+       [(eq? char #\!) (values 'comment #f)]
        [else (values 'inv #f)])]
+    ['comment (cond
+       [(eq? char #\newline) (values 'start 'comment)]
+       [else (values 'comment #f)])
     [else (values 'inv #f)]))
