7b720d0eb1cd0e495a40aa1f4896d3e11d8ed60b
Tarazonaa <andres.tara.so@gmail.com>
2023-05-31 21:25:29 -0600

    Add all three files. Start work on lexer

---
+++ lexer/lexer.lua
@@ -0,0 +1,34 @@
+print()
+print("Hi")
+
+-- comment
+t = {}
+t = { a = 1, b = 2 }
+
+while condition do
+end
+
+for i = 1,5 do
+end
+
+for v in pairs(t) do
+end
+
+repeat
+until condition
+
+if condition then
+  print("yes")
+elseif condition then
+  print("maybe")
+else
+  print("no")
+end
+
+function f()
+end
+
+function f(a, b)
+end
+
+
